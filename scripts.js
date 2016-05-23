fa00a1b91f2b0e58a63533d1c439d1de49836558
Msrcin <kurus.mk@gmail.com>
2016-05-23 16:39:27 +0200

    gallery

--- scripts.js
+++ scripts.js
@@ -2,4 +2,24 @@
  * Created by kurus.mk on 19.05.2016.
  */
 
-$("#about").animate({ scrollTop: y + $(window).height() }, 600);
+/ $(document).ready(function () {
+     var gallery = 0;
+     rotate();
+     function rotate() {
+         var adImages = [
+             "images/photo1.jpg",
+             "images/photo2.jpg",
+             "images/photo3.jpg",
+             "images/photo4.jpg",
+             "images/photo5.jpg",
+             "images/photo6.jpg"
+         ];
+
+         gallery++;
+         if (gallery == adImages.length) {
+             gallery = 0;
+         }
+        $('body').attr('src', galeria[1]);
+         setTimeout(rotate, 6 * 1000);
+     }
+ });
